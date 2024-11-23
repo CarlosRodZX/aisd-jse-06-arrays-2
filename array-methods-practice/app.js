f1762b019c1b3ab1f669b4a2125186e1999acb46
CharZX1 <sonicadvanced21@hotmail.com>
2024-11-23 10:44:31 -0800

    Array Methods Exercise

---
+++ array-methods-practice/app.js
@@ -0,0 +1,67 @@
+// Array of numbers 
+
+const nunmbers = [5, 12, 8, 130, 44];
+
+// Filter number greater than 10
+const filteredNumbers = nunmbers.filter
+((num) => num > 10); 
+
+console.log("Filtered Numbers Greater than 10):", filteredNumbers); // Output: [12, 130, 44]
+
+// Sort numbers in ascending order 
+
+const sortedNumbers = nunmbers.sort
+
+((a, b) => a - b);
+
+console.log("Sorted Numbers Ascending Order):", sortedNumbers);
+
+// Output: [5, 8, 12, 44, 130]
+
+// Check if all numbers are greater than 3 
+
+const allGreaterThanThree = nunmbers.every((num) => num > 3);
+
+console.log("Are all numbers greater than 3?", allGreaterThanThree);
+
+// Output: true
+
+// Array of words
+
+const words = ["banana", "strawberry", "kiwi", "grape", "apple"];
+
+// Filter words longer than 5 characters
+
+const longWords = words.filter((word) => word.length > 5);
+
+console.log("Words Longer Than 5 Characters:", longWords);
+
+// Output: ['banana', 'strawberry']
+
+
+
+// Sort words alphabetically 
+
+const sortedWords = words.sort();
+
+console.log("Alphabetically Sorted Words:", sortedWords); 
+
+// Output: ["apple", "banana",'grape', 'kiwi', 'strawberry']
+
+
+// Check if all words contain the letter "e"
+
+const allContainE = words.every
+((word) => word.includes("e"));
+
+console.log('Do all words contain the letter "e"?', allContainE);
+
+// Output: true
+
+// Check if all the words are shorter than 10 characters 
+
+const allShoterThan10 = words.every((word) => word.length < 10);
+
+console.log("Are all words shorter than 10 characterts?", allShoterThan10);
+
+// Output: false
